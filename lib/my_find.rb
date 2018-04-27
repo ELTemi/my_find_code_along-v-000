45f0d6c1e1f9955406c972a0bfe69112e210d023
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-04-27 21:54:24 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,9 +2,8 @@ require 'pry'
 
 def my_find(collection)
   i = 0
-  new_return = []
   while (i < collection.length)
-    new_return << yield(collection[i])
+    yield(collection[i])
     i += 1
   end
 end
